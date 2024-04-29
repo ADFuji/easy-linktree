6d8a815589668b2dbb5db070b0dac2722f1dc57f
Adrien Moutet <adrien.moutetcvl@gmail.com>
2024-04-29 22:53:24 +0200

    fix copy sur safari

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -15,6 +15,9 @@ export default function Link({
   return (
         <div className="Link">
             <div className="Link-icon">
+                <img src={
+                    `https://www.google.com/s2/favicons?sz=256&domain=${href}`
+                } alt={title} />
             </div>
 
             <div className="Link-content">
@@ -25,15 +28,30 @@ export default function Link({
             <div className="Link-cta">
                 <IconButton onClick={() => {
                         window.open(href, '_blank')
+                        setNotifications(notifications => [...notifications, <Notification key={notifications.length}
+                            setNotifications={setNotifications}
+                            message={`Lien ouvert : ${href}`} />])
                     }}>
                     <IoNavigate />
                 </IconButton>
                 <IconButton onClick={() => {
+                    // si le navigateur est safari
+                    if (navigator.vendor.includes('Apple')) {
+                        // copier dans le presse-papier
+                        // va savoir pourquoi ça marche mais on va pas se plaindre
+                        const el = document.createElement('textarea')
+                        el.value = href
+                        document.body.appendChild(el)
+                        el.select()
+                        document.execCommand('copy')
+                        document.body.removeChild(el)
+                    } else {
                         navigator.clipboard.writeText(href)
-                        setNotifications(notifications => [...notifications, <Notification key={notifications.length} 
-                            setNotifications={setNotifications}
-                            message={`Lien copié : ${href}`} />])
-                    }}>
+                    }
+                    setNotifications(notifications => [...notifications, <Notification key={notifications.length} 
+                        setNotifications={setNotifications}
+                        message={`Lien copié : ${href}`} />])
+                }}>
                     <FaCopy />
                 </IconButton>
             </div>
